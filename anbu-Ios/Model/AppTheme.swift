4c9964b3d15aaf6e7ab20efbe8c26947263d7486
anbu <anbu@thelaunchclub.in>
2023-10-19 14:47:24 +0530

    Revert "third commit"
    This reverts commit a0f5059d872721d890f3ed75f2c06f4a7330bb2a.

---
+++ anbu-Ios/Model/AppTheme.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+
+class AppTheme {
+    
+    public static let lightThemeColor: String = "LightThemeColor"
+    public static let darkThemeColor: String = "DarkThemeColor"
+    public static let secondary: String = "Secondary"
+    public static let activeColor: String = "ActiveColor"
+    public static let primary: String = "Primary"
+    public static let tertiaryColor: String = "Tertiary"
+    public static let notificationBackgroundColor: String = "NotificationBackgroundColor"
+    public static let notificationOutlineColor: String = "NotificationOutlineColor"
+    public static let onPrimary: String = "OnPrimary"
+    public static let onPrimaryTint: String = "OnPrimaryTint"
+    public static let titleColor: String = "TitleColor"
+    public static let inputTextColor: String = "InputTextColor"
+    public static let iconTextColor: String = "IconTextColor"
+    public static let errorColor: String = "ErrorColor"
+    public static let searchBarBackgroundColor: String = "SearchBarBackgroundColor"
+    public static let outlineColor: String = "OutlineColor"
+    public static let outlineTintColor: String = "OutlineTintColor"
+    
+    public static let headingTitleFont = Font.custom("Lexend-SemiBold", size: 18)
+    public static let regularFont = Font.custom("Lexend-Regular", size: 16)
+    public static let titleFont = Font.custom("Lexend-SemiBold", size: 16)
+    public static let subTitleFont = Font.custom("Lexend-Medium", size: 16)
+    public static let inputTextFont = Font.custom("Lexend-Medium", size: 15)
+    public static let textButtonFont = Font.custom("Lexend-Medium", size: 13)
+    public static let normalTextFont = Font.custom("Lexend-Regular", size: 13)
+    public static let buttonTitleFont = Font.custom("Lexend-SemiBold", size: 14)
+    public static let mediumTextFont = Font.custom("Lexend-Medium", size: 14)
+    public static let defaultTextFont = Font.custom("Lexend-Regular", size: 14)
+    public static let iconTextFont = Font.custom("Lexend-Medium", size: 11)
+}
+
--- anbu-Ios/hello.swift
+++
@@ -1,8 +0,0 @@
-//
-//  hello.swift
-//  anbu-Ios
-//
-//  Created by Anbu p on 19/10/23.
-//
-
-import Foundation
